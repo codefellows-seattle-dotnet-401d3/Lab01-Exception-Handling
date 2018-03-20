76219696bea685ceb8b1e9d31d2cae53f1fc78e8
Playingtygre <kymiehsu123@gmail.com>
2018-03-20 13:09:59 -0400

    addind questions

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,37 @@ namespace ConsoleApp1
 {
     class Program
     {
+      
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Greetin take this quiz!");
+            QuestionOne();
+
         }
+
+        static void QuestionOne()
+        {
+            int UserAge, UserNum;
+            string Username, UserColor, UserAnimal;
+
+            Console.WriteLine("How old are you ?");
+           UserAge = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("What is your name:?");
+            Username = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite Color?");
+            UserColor = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine(" pick a number 1 or 2");
+            UserNum = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine("Do you like Dogs or Cats?");
+            UserAnimal = Convert.ToString(Console.ReadLine());
+
+         
+        }
+
+       
     }
 }
